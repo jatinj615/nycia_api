83e2bcb91704061d39db69556e45eecfebcb4ebb
Jatin <jatinj615@gmail.com>
2018-01-01 20:45:48 +0530

    user login and signup

--- app.js
+++ app.js
@@ -2,61 +2,89 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var crypt = require('crypto');
+var bcrypt = require('bcrypt');
 var bodyParser = require('body-parser');
 
 
 //database connect
-var url = "mongodb://localhost:27017/Nycia";
-mongoose.createConnection(url,{
-	useMongoClient: true,
-});
+mongoose.connect('mongodb://localhost/nycia');
 
 //bodyparser
 app.use(bodyParser());
 //set viewEngine
 app.set('view engine', 'ejs');
 
+
+//schemas for database
+
+//user schema
 var userSchema = mongoose.Schema({
 	name: String,
 	email: String,
 	password: String,
-	phone_no: String,
+	phone_no: String
 });
+
+//saloon schema
+var saloonSchema = mongoose.Schema({
+
+});
+
+
+
+//models for schemas
+var User = mongoose.model('User',userSchema);
+var Saloon = mongoose.model('Saloon',saloonSchema);
 
 app.get('/',function(req, res){
 	res.render('index');
 });
 
 app.post('/login', function(req, res){
+	//use token for different search for user and saloons
+	var token = 'users';
+	if(token == 'users'){
+		var user = User.findOne({email: req.body.email},function(err,data){
+			if(err) throw err;
+			var hashPass = data.password;
+			if( bcrypt.compareSync(req.body.password,hashPass)){
+				res.end(JSON.stringify(data));
+			}
+		});
+		// console.log(JSON.stringify(user));
+		// res.end(user);		
+	}else if(token == 'saloons'){
 
-	console.log(req.body);
-	var user = {
-		user: {
-			username: req.body.name,
-			password: req.body.lastname
-		}
-	};
-	res.end(JSON.stringify(user));
-	console.log("works");
-	// res.end("working");
-
+	}
 });
 
 app.post('/signup',function(req, res){
+	//use token for different signup for user and saloons
+	var token = 'users'
+	// console.log(req.body);
+	if(token == 'users'){
+		var user = req.body;
+		var hashPass = bcrypt.hashSync(user.password, 10);
+		var item = User({
+			name: user.name,
+			email: user.email,
+			phone_no: user.phone_no,
+			password: hashPass
+		});
+		item.save(function(err){
+			
+			if(err){
+				res.end('exists');
+			}else{
+				console.log('saved');
+				res.end(JSON.stringify(item));
+			}
+		});
+	}else if(token == 'saloons'){
 
-	mongodb.connect(url, function(err, db) {
-	  if (err) throw err;
-	  const pass = crypt.createHash('sha256', 'password123')
-	  // console.log(pass);
-	  var user = {name: "Jatin", password: pass};
-	  db.collection("Users").insertOne(user, function(err,data){
-	  	if(err) throw err;
-	  	console.log(data.password);
-	  	db.close();
-	  });
-	});
-
+	}
+	
+	
 });
 
 app.listen(4000);
